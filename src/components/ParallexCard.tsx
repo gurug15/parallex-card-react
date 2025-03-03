f112053af0e9f168286bc7b956d61b89e77d4a13
GURUGASTGAR <gurugastgar@gmail.com>
2025-03-03 20:49:49 +0530

    added parallex effect

--- src/components/ParallexCard.tsx
+++ src/components/ParallexCard.tsx
@@ -1,13 +1,15 @@
-
+import Tilt from 'react-parallax-tilt'
 
 export default function ParallexCard({image,heading,subheading}:{image?:string,heading?:string,subheading?:string}) {
 
   return (
+    <Tilt>
     <div className="h-96 w-60 bg-black relative rounded-lg overflow-hidden flex flex-col shadow-[3px_3px_10px_rgba(0,0,0,0.5)] border-[1px] border-white/20">
      <img src={image} alt={heading} className="h-full w-full object-cover absolute inset-0 z-0"/>
      <div className="absolute inset-0 z-0" style={{background: "linear-gradient(to top, #000, transparent)"}}></div>
      <h2 className="text-sm z-10 relative mt-auto mx-auto text-white/80 ">{heading}</h2>
-     <p className=" text-xs z-10 relative mx-auto mb-5 text-white/60 font-light">{subheading}</p>
+     <p className=" text-xs z-10 relative mx-auto mb-5 text-white/60 font-light mt-1">{subheading}</p>
     </div>
+    </Tilt>
   )
 }
